6c2285c36257d1c6d532457523dfa1297a50e378
André Mello <andremello91@gmail.com>
2020-08-21 19:49:44 -0400

    Separated operators from main.

--- OnPlaneComponents/Force/Force.cs
+++ OnPlaneComponents/Force/Force.cs
@@ -1,23 +1,12 @@
 ﻿using System;
-using MathNet.Numerics;
-using UnitsNet;
 using UnitsNet.Units;
 
 namespace OnPlaneComponents
 {
 	/// <summary>
-	/// Directions
-	/// </summary>
-	public enum Direction
-	{
-		X = 1,
-		Y = 2
-	}
-
-	/// <summary>
 	/// Force struct.
 	/// </summary>
-	public struct Force : IEquatable<Force>
+	public partial struct Force : IEquatable<Force>
 	{
 		// Auxiliary fields
 		private UnitsNet.Force _forceX, _forceY;
@@ -196,55 +185,5 @@ namespace OnPlaneComponents
 		}
 
         public override int GetHashCode() => (int) (ComponentX * ComponentY);
-
-		/// <summary>
-        /// Returns true if components are equal.
-        /// </summary>
-		public static bool operator == (Force left, Force right) => left.Equals(right);
-
-		/// <summary>
-		/// Returns true if components are different.
-		/// </summary>
-		public static bool operator != (Force left, Force right) => !left.Equals(right);
-
-        /// <summary>
-        /// Returns a <see cref="Force"/> object with summed components, in left argument's unit.
-        /// </summary>
-        public static Force operator + (Force left, Force right) => new Force(left._forceX + right._forceX, left._forceY + right._forceY, left.Unit);
-
-        /// <summary>
-        /// Returns a <see cref="Force"/> object with subtracted components, in left argument's unit.
-        /// </summary>
-        public static Force operator - (Force left, Force right) => new Force(left._forceX - right._forceX, left._forceY - right._forceY, left.Unit);
-
-        /// <summary>
-        /// Returns a <see cref="Force"/> object with multiplied components by a <see cref="double"/>.
-        /// </summary>
-        public static Force operator * (Force force, double multiplier) => new Force(multiplier * force._forceX, multiplier * force._forceY, force.Unit);
-
-        /// <summary>
-        /// Returns a <see cref="Force"/> object with multiplied components by a <see cref="double"/>.
-        /// </summary>
-        public static Force operator * (double multiplier, Force force) => force * multiplier;
-
-        /// <summary>
-        /// Returns a <see cref="Force"/> object with multiplied components by an <see cref="int"/>.
-        /// </summary>
-        public static Force operator * (Force force, int multiplier) => force * (double) multiplier;
-
-        /// <summary>
-        /// Returns a <see cref="Force"/> object with multiplied components by an <see cref="int"/>.
-        /// </summary>
-        public static Force operator * (int multiplier, Force force) => force * (double) multiplier;
-
-        /// <summary>
-        /// Returns a <see cref="Force"/> object with components divided by a <see cref="double"/>.
-        /// </summary>
-        public static Force operator / (Force force, double divider) => new Force(force._forceX / divider, force._forceY / divider, force.Unit);
-
-        /// <summary>
-        /// Returns a <see cref="Force"/> object with components divided by an <see cref="int"/>.
-        /// </summary>
-        public static Force operator / (Force force, int divider) => force / (double) divider;
 	}
 }
